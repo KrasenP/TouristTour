ed0bd226c1944878e8383f1185ed3f97e93bc457
KrasenPenev <krasennikolicov@abv.bg>
2023-08-19 03:09:06 +0300

    sm

---
+++ TouristToursAppWeb.Web.ViewModel/TourBokingFormViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static TouristToursAppWeb.Common.EntityValidationConstants.TourBooking;
+
+namespace TouristToursAppWeb.Web.ViewModel
+{
+    public class TourBokingFormViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime BookedDate { get; set; }
+
+        [Required]
+        [Range(1,50)]
+        public int CountOfPeople { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$")]
+        public string PhoneNumber { get; set; }
+
+        public Guid TourId { get; set; }
+
+        public Guid BookingUserId { get; set; }
+    }
+}
---
+++ TouristToursAppWeb/Controllers/TourBookingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TouristToursAppWeb.Web.ViewModel;
+
+namespace TouristToursAppWeb.Controllers
+{
+    public class TourBookingController : BaseController
+    {
+        public IActionResult MakeBooking()
+        {
+            var model =  new TourBokingFormViewModel();
+            return View(model);
+        }
+
+
+    }
+}
